7928f56004da41b3c6dd428319922e3f35b543cd
Diego Cascante <diego.cascante@hotmail.com>
2024-04-03 16:27:23 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Double Click Event Issue',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
